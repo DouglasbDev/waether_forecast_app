052233d0927e33ebafe428420923d7c444f6760c
douglas.biserra <douglas.beserra@hotmail.com>
2022-10-17 13:52:08 -0300

    melhorias datasourceIMPL

--- lib/modules/search/external/datasource/weather_datasource.dart
+++ lib/modules/search/external/datasource/weather_datasource.dart
@@ -2,14 +2,21 @@ import 'package:dio/dio.dart';
 import 'package:waether_forecast_app/modules/search/domain/entities/weather_entity.dart';
 
 import '../../infra/datasources/i_weather_datasource.dart';
+import '../../infra/mappers/weather_mapper.dart';
 
 class WeatherDatasource extends IWeatherDatasource {
-  final Dio dio;
+  final Dio _dio;
 
-  WeatherDatasource(this.dio);
+  WeatherDatasource(this._dio);
   @override
   Future<List<WeatherEntity>> getWeather(String cityName) async {
-    final response =
-        await dio.get('https://goweather.herokuapp.com/weather/$cityName');
+     try {
+    var response = await _dio.get('https://goweather.herokuapp.com/weather/$cityName');
+    print(response);
+  } catch (e) {
+    print(e);
   }
+  return [];
+  }
+  
 }
--- lib/modules/search/infra/mappers/weather_mapper.dart
+++ lib/modules/search/infra/mappers/weather_mapper.dart
@@ -2,12 +2,21 @@ import '../../domain/entities/forecast_entity.dart';
 import '../../domain/entities/weather_entity.dart';
 
 class WeatherMapper {
-  static WeatherEntity fromMap(Map<String, dynamic> data) {
+  static WeatherEntity fromMap(Map<String, dynamic> map) {
     return WeatherEntity(
-      temperature: data['temperature'] as String,
-      wind: data['wind'] as String,
-      description: data['description'] as String,
-      forecast: data['forecast'] as List<ForecastEntity>,
+      temperature: map['temperature'] as String,
+      wind: map['wind'] as String,
+      description: map['description'] as String,
+      forecast: map['forecast'] as List<ForecastEntity>,
     );
+  }
+
+  static Map<String, dynamic> toMap(WeatherEntity weather) {
+    return <String, dynamic>{
+      'temperature': weather.temperature,
+      'wind': weather.wind,
+      'description': weather.description,
+      'forecast': weather.forecast
+    };
   }
 }
