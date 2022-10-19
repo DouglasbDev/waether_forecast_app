7610f5b2cf5c0495a1fbfc9cd7e69e6d3abc6794
Douglas Biserra <douglas.beserra13@gmail.com>
2022-10-19 06:59:32 -0300

    melhorias datasource

--- lib/modules/search/external/datasource/weather_datasource.dart
+++ lib/modules/search/external/datasource/weather_datasource.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:dio/dio.dart';
 import 'package:waether_forecast_app/modules/search/domain/entities/weather_entity.dart';
 import '../../infra/datasources/weather_datasource_interface.dart';
@@ -8,13 +10,17 @@ class WeatherDatasource extends IWeatherDatasource {
   WeatherDatasource(this._dio);
   @override
   Future<List<WeatherEntity>> getWeather(String cityName) async {
-    try {
-      var response =
-          await _dio.get('https://goweather.herokuapp.com/weather/$cityName');
-      print(response);
-    } catch (e) {
-      print(e);
-    }
-    return [];
+    final response =
+        await _dio.get('https://goweather.herokuapp.com/weather/$cityName');
+    return response.data;
+    // try {
+    //   final response =
+    //       await _dio.get('https://goweather.herokuapp.com/weather/$cityName');
+
+    //   print(response);
+    // } catch (e) {
+    //   print(e);
+    // }
+    // return [];
   }
 }
